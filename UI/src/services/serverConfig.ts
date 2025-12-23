e2abca1c1685a05503f45f3e19b52a86e4fba0b9
Nirmal <nirmaljyothisbenny97@gmail.com>
2025-12-23 16:28:16 +0000

    api url update

--- UI/src/services/serverConfig.ts
+++ UI/src/services/serverConfig.ts
@@ -1,5 +1,7 @@
 const config = {
-  API_URL: import.meta.env.VITE_API_URL,
+  API_URL:
+    // import.meta.env.VITE_API_URL ||
+    "https://erpproject-development.up.railway.app/api",
 };
 
 export default config;
