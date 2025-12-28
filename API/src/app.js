a31fb5b780a6e721878da916e6f4c547505257b1
Nirmal <nirmaljyothisbenny97@gmail.com>
2025-12-28 20:31:42 +0000

    backend url updated in UI

--- API/src/app.js
+++ API/src/app.js
@@ -13,12 +13,7 @@ import notificationRoutes from "./routes/notificationRoutes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
--- UI/src/services/serverConfig.ts
+++ UI/src/services/serverConfig.ts
@@ -1,7 +1,6 @@
 const config = {
-  API_URL:
-    // import.meta.env.VITE_API_URL ||
-    "https://erpproject-development.up.railway.app/api",
+  // API_URL: import.meta.env.VITE_API_URL,
+  API_URL: "https://erpproject-development.up.railway.app/api",
 };
 
 export default config;
