779a9332bcdfb074615ece86805ad94103e8cfca
Nirmal <nirmaljyothisbenny97@gmail.com>
2026-07-16 11:13:26 +0100

    cors fix

--- API/src/app.js
+++ API/src/app.js
@@ -19,17 +19,9 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: allowedOrigins,
     credentials: true,
-  })
+  }),
 );
 
 app.use(express.json());
