e0a64affef51c9235a2edfeb0e80ff446a387cf6
Nirmal <nirmaljyothisbenny97@gmail.com>
2026-07-16 09:58:49 +0100

    adding deployemnt files

--- API/src/app.js
+++ API/src/app.js
@@ -14,6 +14,7 @@ import notificationRoutes from "./routes/notificationRoutes.js";
 const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://your-erp-app.vercel.app",
 ];
 
 app.use(
